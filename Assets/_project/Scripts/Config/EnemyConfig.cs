3c633e7997bdd194399ee4faf58fbd68e7d14b3f
Charles Carter <charles.c@playwindgames.com>
2023-02-04 21:02:54 +0000

    feat: enemy groups config

--- Assets/_project/Scripts/Config/EnemyConfig.cs
+++ Assets/_project/Scripts/Config/EnemyConfig.cs
@@ -6,6 +6,7 @@ public class EnemyConfig
 {
     public Dictionary<string, EnemyTemplate> enemies = new Dictionary<string, EnemyTemplate>();
     public Dictionary<string, EnemyWeaponTemplate> enemyweapons = new Dictionary<string, EnemyWeaponTemplate>();
+    public Dictionary<string, EnemyGroupTemplate> enemyGroups = new Dictionary<string, EnemyGroupTemplate>();
 
     public EnemyConfig()
     {
---
+++ Assets/_project/Scripts/Config/Templates/EnemyGroupTemplate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroupTemplate
+{
+    public List<string> enemiesInGroup { get; set; }
+    public float weight { get; set; }
+
+    public EnemyGroupTemplate()
+    {
+
+    }
+}
