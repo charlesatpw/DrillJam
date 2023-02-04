708e535c9ec423f2f7acb55c3b7e28b47600768a
Charles Carter <charles.c@playwindgames.com>
2023-02-04 12:50:18 +0000

    chore: adding ui scripts

--- Assets/_project/Scripts/SoundManager.cs
+++ Assets/_project/Scripts/SoundManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenu : MonoBehaviour
+public class SoundManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
