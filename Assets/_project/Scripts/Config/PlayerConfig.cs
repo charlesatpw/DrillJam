e67ac02169867f6b4e5944ed5d6af393b36cb240
Charles Carter <charles.c@playwindgames.com>
2023-02-04 15:29:52 +0000

    config: player max health/fuel

--- Assets/_project/Scripts/Config/PlayerConfig.cs
+++ Assets/_project/Scripts/Config/PlayerConfig.cs
@@ -1,10 +1,10 @@
 public class PlayerConfig 
 {
     public float turnSpeed { get; set; }
-
     public float forwardSpeed { get; set; }
-
     public float maxRotationAngle { get; set; }
+    public int maxFuel { get; set; }
+    public int maxHealth { get; set; }
 
     public PlayerConfig()
     {
