5e2e4b7a5b9b724394f204bfa5eb04400ba4e989
Charles Carter <charles.c@playwindgames.com>
2023-02-04 21:51:16 +0000

    perf: item config includes placement

--- Assets/_project/Scripts/Config/ItemConfig.cs
+++ Assets/_project/Scripts/Config/ItemConfig.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ItemConfig
 {
+    public int maxTriesPerSpawn { get; set; }
+    public int numOfItemsPerSection { get; set; }
+    public int itemSectionCount { get; set; }
     public Dictionary<string, ItemTemplate> items = new Dictionary<string, ItemTemplate>();
 
     public ItemConfig() { }
